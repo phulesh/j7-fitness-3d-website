915a9b308b0a125bb0c157df18e9b4047e69df18
Phuleshwar Nishad <124109641+phulesh@users.noreply.github.com>
2026-08-13 07:07:53 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['j7-fitness-3d-website-production.up.railway.app']
-  }
-})
